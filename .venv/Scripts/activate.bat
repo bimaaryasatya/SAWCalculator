88d2c3e410b74e68b7a9e4d14a2e89004382a297
bimaaryasatya <145569118+bimaaryasatya@users.noreply.github.com>
2025-06-24 00:58:02 +0700

    venv: reinstall

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\bimazznxt\Desktop\AplikasiSAW\.venv
+set VIRTUAL_ENV=C:\Users\BIMAZZNXT\Documents\SAWCalculator\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
